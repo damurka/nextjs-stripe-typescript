43e3f4303f83cafe965e4c7f3786a6f1bab324e0
Damurka <kdamurka@gmail.com>
2020-09-03 23:09:21 +0300

    Change to typescript and add checkout

---
+++ pages/checkout.tsx
@@ -0,0 +1,28 @@
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+
+const handleClick = async (event) => {
+    const { sessionId } = await fetch('/api/checkout/session', {
+        method: 'POST',
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({ quantity: 1})
+    }).then(res => res.json());
+    const stripe = await stripePromise;
+    const { error } = await stripe.redirectToCheckout({
+        sessionId
+    });
+};
+
+const Checkout = () => (
+    <div>
+        <h1>Checkout</h1>
+        <button role="link" onClick={handleClick}>
+            Checkout
+        </button>
+    </div>
+);
+
+export default Checkout;
